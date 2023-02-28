a022cb4367aaf8f4c49254802895cdbf7b7c7fe4
tommaso <codicem69@gmail.com>
2023-02-28 16:58:29 +0100

    modifica customizzazione model shipsteps con assegnazione batch_assign per poter riassegnare l'id agenzia

--- packages/agz/model/_packages/shipsteps/email_services.py
+++ packages/agz/model/_packages/shipsteps/email_services.py
@@ -1,5 +1,5 @@
 class Table(object):
     def config_db(self,pkg):
         tbl=pkg.table('email_services', partition_agency_id='agency_id')
-        tbl.column('agency_id',size='22', name_long='agency_id'
+        tbl.column('agency_id',size='22', name_long='agency_id',batch_assign=True
                     ).relation('agz.agency.id', relation_name='email_services_sh', mode='foreignkey', onDelete='raise')
--- packages/agz/model/_packages/shipsteps/protocollo.py
+++ packages/agz/model/_packages/shipsteps/protocollo.py
@@ -3,5 +3,5 @@ class Table(object):
 
         tbl=pkg.table('protocollo', partition_agency_id='agency_id')
 
-        tbl.column('agency_id',size='22',name_long='!![en]Agency').relation(
+        tbl.column('agency_id',size='22',name_long='!![en]Agency', batch_assign=True).relation(
                                     'agz.agency.id',relation_name='agency_shprot', mode='foreignkey', onDelete='raise')
