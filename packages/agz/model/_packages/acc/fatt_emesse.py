9f0fb30c44e4ecd673d3713ce3bd6ff866652c2e
tommaso <codicem69@gmail.com>
2023-04-17 09:00:25 +0200

    aggiunto customizzazione model acc.fatt_emesse

---
+++ packages/agz/model/_packages/acc/fatt_emesse.py
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+class Table(object):
+    def config_db(self,pkg):
+        tbl=pkg.table('fatt_emesse', partition_agency_id='agency_id')
+        tbl.column('agency_id',size='22', name_long='!![en]agency', plugToForm=dict(hasDownArrow=True)
+                    ).relation('agz.agency.id', relation_name='agency_fat_emesse', mode='foreignkey', onDelete='setNull')
