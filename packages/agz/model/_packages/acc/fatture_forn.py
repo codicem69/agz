14f4f0e4979a06e14b97f7f685f38f02ae6c31d0
tommaso <codicem69@gmail.com>
2023-04-28 18:31:29 +0200

    modifica fat_emesse e fatture_forn

--- packages/agz/model/_packages/acc/fatture_forn.py
+++ packages/agz/model/_packages/acc/fatture_forn.py
@@ -3,5 +3,5 @@
 class Table(object):
     def config_db(self,pkg):
         tbl=pkg.table('fatture_forn',partition_agency_id='agency_id')
-        tbl.column('agency_id',size='22', name_long='!![en]agency'
+        tbl.column('agency_id',size='22', name_long='!![en]agency',batch_assign=True
                     ).relation('agz.agency.id', relation_name='agency_fat_forn', mode='foreignkey', onDelete='setNull')
