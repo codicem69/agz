0d871e4bbc9822cb54effd0539f170403a30a3b5
tommaso <codicem69@gmail.com>
2023-12-27 15:06:34 +0100

    inserito autorizzazione con scelta data al login anche per gli utenti user

--- packages/agz/resources/login.py
+++ packages/agz/resources/login.py
@@ -4,6 +4,9 @@ from gnr.web.gnrwebpage import BaseComponent
         
 class LoginComponent(BaseComponent):
     
+    auth_workdate = 'admin,user' #questa riga ci permette di avere la data di lavoro nel login anche per gli user così in caso di
+                                 #inserire un record con protocollo datato in un anno differente possiamo al login decidere la data di login
+
     def onAuthenticating_agz(self,avatar,rootenv=None):
         agency_id = self.db.table('agz.staff').readColumns(where='$user_id=:u_id',
                             u_id=avatar.user_id, columns='$agency_id')
@@ -12,4 +15,3 @@ class LoginComponent(BaseComponent):
 
         rootenv['current_agency_id'] = agency_id 
         rootenv['current_port'] = port
-
