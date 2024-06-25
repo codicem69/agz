7db74ba826cfcab45f3421656b063dc7322fb41e
tommaso <codicem69@gmail.com>
2024-06-25 18:25:44 +0200

    modifica relazione agency_id nella customizzazione del model di adminchargerec

--- packages/agz/model/_packages/shipsteps/adminchargerec.py
+++ packages/agz/model/_packages/shipsteps/adminchargerec.py
@@ -5,4 +5,4 @@ class Table(object):
         tbl=pkg.table('adminchargerec', partition_agency_id='agency_id')
 
         tbl.column('agency_id',size='22',name_long='!![en]Agency',batch_assign=True).relation(
-                                    'agz.agency.id',relation_name='agency_arrival', mode='foreignkey', onDelete='raise')
+                                    'agz.agency.id',relation_name='agency_admincharge', mode='foreignkey', onDelete='raise')
