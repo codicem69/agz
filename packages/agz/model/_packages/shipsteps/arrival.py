9abe6b73aa08ff5fb2277b33dd7627852eb34649
codicem69 <codicem69@gmail.com>
2025-02-26 11:06:31 +0100

    aggiunto customizzazione risorsa email per partizionamento in base all'agenzia

--- packages/agz/model/_packages/shipsteps/arrival.py
+++ packages/agz/model/_packages/shipsteps/arrival.py
@@ -4,4 +4,4 @@ class Table(object):
         tbl=pkg.table('arrival', partition_agency_id='agency_id')
 
         tbl.column('agency_id',size='22',name_long='!![en]Agency',batch_assign=True).relation(
-                                    'agz.agency.id',relation_name='agency_arrival', mode='foreignkey', onDelete='raise')
+                                    'agz.agency.id',relation_name='agency_email', mode='foreignkey', onDelete='raise')
