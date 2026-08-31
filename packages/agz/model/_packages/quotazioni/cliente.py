e7b56ce872d54ddac94085f2c6c2855520f688d1
codicem69 <codicem69@gmail.com>
2026-08-31 14:45:02 +0200

    modificato tabella cliente quotazioni

--- packages/agz/model/_packages/quotazioni/cliente.py
+++ packages/agz/model/_packages/quotazioni/cliente.py
@@ -3,5 +3,9 @@
 class Table(object):
     def config_db(self,pkg):
         tbl=pkg.table('cliente', partition_agency_id='agency_id')
+        self.sysFields(tbl)
         tbl.column('agency_id',size='22',name_long='!![en]Agency',batch_assign=True).relation(
-                                    'agz.agency.id',relation_name='cliente_quotazione', mode='foreignkey', onDelete='raise')
+                                    'agz.agency.id',relation_name='agency_cliente', mode='foreignkey', onDelete='setNull')
+
+    def defaultValues(self):
+        return dict(agency_id=self.db.currentEnv.get('current_agency_id'))
